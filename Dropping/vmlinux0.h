0e428b167e01b2bfcf318f02605cf747f74cdde7
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2023-11-18 02:20:03 +0000

    Update vmlinux0.h

--- Dropping/vmlinux0.h
+++ Dropping/vmlinux0.h
@@ -1,6 +1,14 @@
-#define ETH_P_IP 0x0800
+#ifndef BPF_MAP_TYPE_RINGBUF
 #define BPF_MAP_TYPE_RINGBUF 27
+#endif
+
+#ifndef BPF_RB_FORCE_WAKEUP
 #define BPF_RB_FORCE_WAKEUP 2
+#endif
+
+#ifndef ETH_P_IP
+#define ETH_P_IP 0x0800
+#endif
 
 struct xdp_md {
   __u32 data;
