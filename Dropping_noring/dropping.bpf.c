8bfa61b11162bcf750d9359dc74588df69803f62
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2023-11-19 18:20:26 +0000

    Update dropping.bpf.c

--- Dropping_noring/dropping.bpf.c
+++ Dropping_noring/dropping.bpf.c
@@ -14,50 +14,45 @@ struct {
 
 struct {
   __uint(type, BPF_MAP_TYPE_ARRAY);
-	__type(key, uint16_t);
-	__type(value, struct pingmsg_t);
-	__uint(max_entries, 1024);
+  __type(key, uint16_t);
+  __type(value, struct pingmsg_t);
+  __uint(max_entries, 1024);
 } pingtraffic_array SEC(".maps");
 
 SEC("xdp")
 int processping(struct xdp_md *ctx) {
-  void *data_end = (void *)(long)ctx->data_end;
-  void *data = (void *)(long)ctx->data;
-  struct pingmsg_t msg = {0};
-  static uint16_t skey = 0;
-  
-  struct ethhdr *eth = (struct ethhdr *)data;
-  if ((void*)eth + sizeof(struct ethhdr) > data_end)
-    return XDP_ABORTED;
-  if (bpf_ntohs(eth->h_proto) != ETH_P_IP)
-    return XDP_PASS;
-
-  struct iphdr* iph = (void*)eth + sizeof(struct ethhdr);
-  if ((void*)iph + sizeof(struct iphdr) > data_end)
-    return XDP_ABORTED;
-
-  if (iph->protocol != IPPROTO_ICMP)
-    return XDP_PASS;
-    
-  if (iph->protocol == IPPROTO_ICMP) {
-    msg.timestamp = bpf_ktime_get_ns();
-    msg.saddr = iph->saddr;
-    msg.daddr = iph->daddr;
-
-    uint16_t key = skey % 1024
-    int ret = bpf_map_update_elem(pingtraffic_array, &key, sizeof(uint32_t), &msg, sizeof(struct pingmesg_t), BPF_ANY);
-    if (ret) {
-      fprintf(stderr, "failed to update element in pingtraffic_array\n");
-      return XDP_ABORTED;
-    }
-    skey++;
-
-    if (bpf_map_lookup_elem(&dropping_hash, &iph->saddr))
-      return XDP_DROP;
-
-  }
-    
-  return XDP_PASS;
+	void *data_end = (void *)(long)ctx->data_end;
+	void *data = (void *)(long)ctx->data;
+	static skey = 0;
+	
+	struct ethhdr *eth = (struct ethhdr *)data;
+	if ((void*)eth + sizeof(struct ethhdr) > data_end)
+		return XDP_ABORTED;
+	if (bpf_ntohs(eth->h_proto) != ETH_P_IP)
+		return XDP_PASS;
+	
+	struct iphdr* iph = (void*)eth + sizeof(struct ethhdr);
+	if ((void*)iph + sizeof(struct iphdr) > data_end)
+		return XDP_ABORTED;
+	if (iph->protocol != IPPROTO_ICMP)
+		return XDP_PASS;
+	
+	if (iph->protocol == IPPROTO_ICMP) {
+		struct pingmg_t msg;
+		msg.timestamp = bpf_ktime_get_ns();
+		msg.saddr = iph->saddr;
+		msg.daddr = iph->daddr;
+		uint16_t key = skey % 1024
+		int ret = bpf_map_update_elem(pingtraffic_array, &key, sizeof(uint16_t), &msg, sizeof(struct pingmsg_t), BPF_ANY);
+		if (ret) {
+			fprintf(stderr, "failed to update element in pingtraffic_array\n");
+			return XDP_ABORTED;
+		}
+		skey++;
+		
+		if (bpf_map_lookup_elem(&dropping_hash, &iph->saddr))
+			return XDP_DROP;
+	}	
+	return XDP_PASS;
 }
-
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
