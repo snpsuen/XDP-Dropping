ebf7d7bc2a9552b562ab87a8cfacae7a3ad4fe8f
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2023-11-19 18:36:51 +0000

    Update vmlinux0.h

--- Dropping_noring/vmlinux0.h
+++ Dropping_noring/vmlinux0.h
@@ -12,6 +12,22 @@
 #define ETH_P_IP 0x0800
 #endif
 
+#ifndef BPF_ANY
+#define BPF_ANY 0
+#endif
+
+#ifndef BPF_NOEXIST
+#define BPF_NOEXIST 1
+#endif
+
+#ifndef BPF_EXIST
+#define BPF_EXIST 2
+#endif
+
+#ifndef BPF_F_LOCK
+#define BPF_F_LOCK 4
+#endif
+
 struct xdp_md {
   __u32 data;
   __u32 data_end;
